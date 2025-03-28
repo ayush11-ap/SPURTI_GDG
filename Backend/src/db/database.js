c599f99f2d5fb58cbf291c16282f9ce4efc4b57e
Ayush Pawar <ayushap1105@gmail.com>
2025-03-28 19:23:18 +0530

    Update database connection to use environment variable for MongoDB URI

--- Backend/src/db/database.js
+++ Backend/src/db/database.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb://localhost:27017/SpurtiRAS");
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 module.exports = connectDB;
